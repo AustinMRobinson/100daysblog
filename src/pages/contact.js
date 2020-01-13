b5eaf45045e650c0537a0449d7525dbe1c218336
austinmrobinson <austinrobinsondesign@gmail.com>
2020-01-12 23:12:05 -0600

    Final set of updates for the night

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,9 +14,9 @@ const ContactPage = () => {
                 <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="bot-field" />
                     <input type="hidden" name="form-name" value="contact" />
-                    <Input name="name" id="name" label="Full Name" placeholder="Enter your name" type="text" htmlFor="name"></Input>
-                    <Input name="email" id="email" label="Email Address" placeholder="Enter your email address" type="text" htmlFor="email"></Input>
-                    <TextArea name="message" id="message" label="Your Message" placeholder="Enter your message" type="textarea" htmlFor="message"></TextArea>
+                    <Input name="name" id="name" label="Full Name" placeholder="Enter your name" type="text" htmlFor="name" required></Input>
+                    <Input name="email" id="email" label="Email Address" placeholder="Enter your email address" type="text" htmlFor="email" required></Input>
+                    <TextArea name="message" id="message" label="Your Message" placeholder="Enter your message" type="textarea" htmlFor="message" required></TextArea>
                     <Button type="submit">Send Message</Button>
                 </form>
             </Container>
